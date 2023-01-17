72e004d5f3340905888914fe1db442a8a84dd536
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-01-17 13:29:01 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: ''
+  }
 }
 
 module.exports = nextConfig
