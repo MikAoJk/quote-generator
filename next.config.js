4861bddeba8122b14c221751722a6f9fb2866d12
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-01-18 07:39:49 +0100

    Removed NEXT_PUBLIC_BASE_PATH

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  swcMinify: true,
-  images: {
-    loader: 'akamai',
-    path: ''
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
